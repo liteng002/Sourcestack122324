8c23de68baf73a3580592848d0da56763af359d2
965330241@qq.com <965330241@qq.com>
2021-04-23 16:06:33 +0800

    init

---
+++ Sourcestack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sourcestack
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte a = 255;
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
